a68473b15686f4ec68cb124f855095f9cc130bf3
Monjit <david10boro@gmail.com>
2025-01-25 20:34:54 -0700

    added comments

--- HeapSort.py
+++ HeapSort.py
@@ -1,10 +1,13 @@
 def heapify(mylist, n, i):
     max_, left, right = i, 2 * i + 1, 2 * i + 2
-    if left <= n and mylist[left] > mylist[max_]:
+    
+    if left <= n and mylist[left] > mylist[max_]: # Check if left child within bound and greater than current max
         max_ = left
-    if right <= n and mylist[right] > mylist[max_]:
+    
+    if right <= n and mylist[right] > mylist[max_]: # Check if right child within bound and greater than current max
         max_ = right
-    if max_ != i:
+    
+    if max_ != i: # swap and heapify, if largest not root
         mylist[i], mylist[max_] = mylist[max_], mylist[i]
         heapify(mylist, n, max_)
 
@@ -13,7 +16,7 @@ def heapsort(mylist):
     
     for i in range(n // 2, -1, -1):
         heapify(mylist, n - 1, i)
-        
+    
     for i in range(n - 1, -1, -1):
         mylist[i], mylist[0] = mylist[0], mylist[i]
         heapify(mylist, i - 1, 0)
